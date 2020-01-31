2d17699dc5b6d96c22c3e5d4494e4932d10ddb04
alexrazor <alexrazor1337@gmail.com>
2020-02-01 00:52:28 +0200

    File check with error messages, single file compiling

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import subprocess, os, sys
 
-working_dir = os.path.dirname(os.path.realpath(__file__))
-arguments = ["-e3", " -o " + os.path.basename(sys.argv[1]) + "c"," -- ", sys.argv[1]]
-subprocess.call([working_dir + "\luac_mta.exe", arguments])
+if len(sys.argv) > 1:
+    working_dir = os.path.dirname(os.path.realpath(__file__))
+    if os.path.isfile(sys.argv[1]):
+        file_basename = os.path.basename(sys.argv[1])
+        print("Working with single file, compiling to " + file_basename + "c")
+        arguments = ["-e3", " -o " + file_basename + "c"," -- ", sys.argv[1]]
+    else:
+        pass
+    subprocess.call([working_dir + "\luac_mta.exe", arguments])
+else:
+    sys.exit("No file or directory specified.")
