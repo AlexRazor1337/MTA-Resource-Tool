7e902c86868e0b92fb6dc53e7f8401ed42a7371c
alexrazor <alexrazor1337@gmail.com>
2020-02-01 19:06:17 +0200

    Settings loading from json file

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import subprocess, os, sys
+import subprocess, os, sys, json
 from datetime import datetime
 from shutil import copyfile
 
-obfuscation_level = "e3"
-info_level = 0 # 0 - detailed info, 1 - info, 2 - warning, 3 - error
-restricted_extensions = []
+default_config = {'obfuscation_level': 'e3','info_level': 1, 'restricted_extensions': []}
+possible_obfuscation_levels = ["e", "e2", "e3"]
+config = {}
 
+def init_settings():
+    try:
+        with open('settings.json', 'r') as f:
+            config = json.load(f)
+            print(config)
+    except IOError:
+        with open('settings.json', 'w+') as f:
+            json.dump(default_config, f)
+            config = default_config
+    if not config['obfuscation_level'] in possible_obfuscation_levels:
+        print("WARNING: Inappropriate obfuscation level, settings will be reset.")
+        os.remove('settings.json')
+        config = default_config
+    return config
+    
 def compile_file(file_path):
     file_basename = os.path.basename(file_path)
-    if info_level == 0:
+    if config['info_level'] == 0:
         print("INFO: Compiling", file_basename)
     if os.path.isfile(file_basename + "c"):
         file_basename = str(datetime.now().time()).replace(":", ".", -1) + file_basename
-        if info_level < 3:
-            print("WARNING: File with this name already exists, compiling to " + file_basename + " instead")
-    arguments = ["-" + obfuscation_level, " -o " + file_basename + "c"," -- ", file_path]
+        if config['info_level'] < 3:
+            print("WARNING: File with this name already exists, compiling to " + file_basename + " instead!")
+    arguments = ["-" + config['obfuscation_level'], " -o " + file_basename + "c"," -- ", file_path]
     subprocess.call([working_dir + "\luac_mta.exe", arguments])
 
 if len(sys.argv) > 1:
     working_dir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(working_dir)
+    config = init_settings()
     if os.path.isfile(sys.argv[1]):
         file_basename = os.path.basename(sys.argv[1])
-        if info_level < 2:
+        if config['info_level'] < 2:
             print("INFO: Working with single file, compiling to " + file_basename + "c")
         compile_file(sys.argv[1])
     elif os.path.isdir(sys.argv[1]):
@@ -38,7 +54,7 @@ if len(sys.argv) > 1:
             resource_name += " " + str(datetime.now().time()).replace(":", ".", -1) #adds time to folder name to handle name dublicates
             os.mkdir(resource_name)
         os.chdir(resource_name)
-        if info_level < 2:
+        if config['info_level'] < 2:
             print("INFO: Working with folder, compiling to", resource_name)
         for dirPath, dirs, files in os.walk(sys.argv[1]): #walks through everything in root folder
             for dir in dirs:
@@ -54,11 +70,11 @@ if len(sys.argv) > 1:
                         if ext == ".lua":
                             compile_file(dirPath + "\\" + file)
                         else:
-                            if not ext in restricted_extensions:
+                            if not ext in config['restricted_extensions']:
                                 copyfile(dirPath + "\\" + file, file)
-                                if info_level == 0:
+                                if config['info_level'] == 0:
                                     print("INFO: Copying", file)
-                            elif info_level == 0:
+                            elif config['info_level'] == 0:
                                 print("INFO: Not copying", file, "because it has restricted extension.")
 
             if len(dirs) == 0: #when there are no folders inside another folder, it can't iterate through it, so previsios cycle won't run
@@ -73,7 +89,7 @@ if len(sys.argv) > 1:
                             compile_file(dirPath + "\\" + file)
                         else:
                             copyfile(dirPath + "\\" + file, file)
-                            if info_level == 0:
+                            if config['info_level'] == 0:
                                 print("INFO: Copying", file)
         
         os.chdir(working_dir + "\Compiled Resources\\" + "\\" + resource_name)
@@ -85,9 +101,9 @@ if len(sys.argv) > 1:
                 meta_file.write(file_string)
                 meta_file.truncate()       
         else:
-            if info_level < 3:
+            if config['info_level'] < 3:
                 print("WARNING: No meta file was found in the resource directory!")
-    if info_level < 2:
+    if config['info_level'] < 2:
         print("INFO: Compiled successfully.")
 else:
-    sys.exit("No file or directory specified.")
+    sys.exit("ERROR: No file or directory specified!")
