86395d8b5cc7ea520eca553318350927436088d6
alexrazor <alexrazor1337@gmail.com>
2020-02-01 18:33:56 +0200

    Changed name of error_level, added check is file extension is restricted

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from datetime import datetime
 from shutil import copyfile
 
 obfuscation_level = "e3"
-error_level = 0 # 0 - detailed info, 1 - info, 2 - warning, 3 - error
+info_level = 0 # 0 - detailed info, 1 - info, 2 - warning, 3 - error
+restricted_extensions = []
 
 def compile_file(file_path):
     file_basename = os.path.basename(file_path)
-    if error_level == 0:
+    if info_level == 0:
         print("INFO: Compiling", file_basename)
     if os.path.isfile(file_basename + "c"):
         file_basename = str(datetime.now().time()).replace(":", ".", -1) + file_basename
-        if error_level < 3:
+        if info_level < 3:
             print("WARNING: File with this name already exists, compiling to " + file_basename + " instead")
     arguments = ["-" + obfuscation_level, " -o " + file_basename + "c"," -- ", file_path]
     subprocess.call([working_dir + "\luac_mta.exe", arguments])
@@ -21,7 +22,7 @@ if len(sys.argv) > 1:
     os.chdir(working_dir)
     if os.path.isfile(sys.argv[1]):
         file_basename = os.path.basename(sys.argv[1])
-        if error_level < 2:
+        if info_level < 2:
             print("INFO: Working with single file, compiling to " + file_basename + "c")
         compile_file(sys.argv[1])
     elif os.path.isdir(sys.argv[1]):
@@ -37,7 +38,7 @@ if len(sys.argv) > 1:
             resource_name += " " + str(datetime.now().time()).replace(":", ".", -1) #adds time to folder name to handle name dublicates
             os.mkdir(resource_name)
         os.chdir(resource_name)
-        if error_level < 2:
+        if info_level < 2:
             print("INFO: Working with folder, compiling to", resource_name)
         for dirPath, dirs, files in os.walk(sys.argv[1]): #walks through everything in root folder
             for dir in dirs:
@@ -53,9 +54,12 @@ if len(sys.argv) > 1:
                         if ext == ".lua":
                             compile_file(dirPath + "\\" + file)
                         else:
-                            copyfile(dirPath + "\\" + file, file)
-                            if error_level == 0:
-                                print("INFO: Copying", file)
+                            if not ext in restricted_extensions:
+                                copyfile(dirPath + "\\" + file, file)
+                                if info_level == 0:
+                                    print("INFO: Copying", file)
+                            elif info_level == 0:
+                                print("INFO: Not copying", file, "because it has restricted extension.")
 
             if len(dirs) == 0: #when there are no folders inside another folder, it can't iterate through it, so previsios cycle won't run
                 dir_path = dirPath.replace(sys.argv[1], "")
@@ -69,7 +73,7 @@ if len(sys.argv) > 1:
                             compile_file(dirPath + "\\" + file)
                         else:
                             copyfile(dirPath + "\\" + file, file)
-                            if error_level == 0:
+                            if info_level == 0:
                                 print("INFO: Copying", file)
         
         os.chdir(working_dir + "\Compiled Resources\\" + "\\" + resource_name)
@@ -81,9 +85,9 @@ if len(sys.argv) > 1:
                 meta_file.write(file_string)
                 meta_file.truncate()       
         else:
-            if error_level < 3:
+            if info_level < 3:
                 print("WARNING: No meta file was found in the resource directory!")
-    if error_level < 2:
+    if info_level < 2:
         print("INFO: Compiled successfully.")
 else:
     sys.exit("No file or directory specified.")
